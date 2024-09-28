d9d56a7c77aa7e0ad7d7ccbf461627e40fa5b625
sanz-latam <atencion@sanz.la>
2024-09-28 15:59:31 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    const BASE_RUTA = __DIR__.'/../';
+
+    require_once BASE_RUTA.'/vendor/autoload.php';
+    require BASE_RUTA.'app/utilidades.php';
+    require BASE_RUTA.'rutas/enrutador.php';
+?>
