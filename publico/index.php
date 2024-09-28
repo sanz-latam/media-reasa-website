50e27561ef30d1de958497fecd612e4c33b4243f
sanz-latam <atencion@sanz.la>
2024-09-28 16:18:18 -0600

    Delete publico directory

--- publico/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    const BASE_RUTA = __DIR__.'/../';
-
-    require_once BASE_RUTA.'/vendor/autoload.php';
-    require BASE_RUTA.'app/utilidades.php';
-    require BASE_RUTA.'rutas/enrutador.php';
-?>
